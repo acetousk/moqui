df459bb6f4093d09604a14d61943a7f312351f58
Marwan Dessouki <marwan.dessouki@outlook.com>
2022-09-04 00:24:21 +0200

    feat: add app to composable context

--- packages/composables/nuxt/plugin.js
+++ packages/composables/nuxt/plugin.js
@@ -2,8 +2,9 @@ import { integrationPlugin } from '@vue-storefront/core';
 
 const moduleOptions = <%= serialize(options) %>;
 
-export default integrationPlugin(({ integration }) => {
+export default integrationPlugin(({ app, integration }) => {
   integration.configure('moqui', {
+    app,
     ...moduleOptions
     // other options
   });
